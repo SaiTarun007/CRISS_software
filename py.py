10dbd2ca4ea20f051d36903cd8aa95c33b6bc141
Sai Tarun <f20220419@bits.bits-pilani.ac.in>
2023-03-27 00:22:57 +0530

    adding py.py file

---
+++ py.py
@@ -0,0 +1,13 @@
+import socket
+
+serverIP='172.17.29.11'
+serverPORT=6000
+
+serveraddress= (serverIP,serverPORT)
+buffersize=1024
+UDPClientSocket=socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
+
+message="PoOP"
+
+bytestosend=str.encode(message)
+UDPClientSocket.sendto(bytestosend, serveraddress)
